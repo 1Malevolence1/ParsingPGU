aac2f11a1bcdbb2f5a333d6c8c1c7a6fe347cc28
Administrator <danil36rusdanil@mail.ru>
2022-10-16 22:50:46 +0300

    получили файл html

---
+++ main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+
+# url = "https://pgu.ru/slk/card/?f_name=&f_activity=&f_year=&f_spec=3&f_inst="
+#
+# headers = {
+#     "Accept": "*/*",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.2.1501 Yowser/2.5 Safari/537.36"
+#
+# }
+#
+# req = requests.get(url,headers=headers)
+# src =req.text
+
+
+#
+# with open("index.html","w",encoding="utf-8") as file:
+#     file.write(src)
